ac48d0530fea2ca73440f29b0ba9d259c980b1b6
Jack McNary <jack.mcnary@gmail.com>
2022-02-01 15:16:35 -0700

    Pseudo-coding jsx files

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,8 @@
+// import the footer css, linkedin and github
+
+// export const Footer
+// div with the classname pertaining to the footer
+// icons, I guess, with an anchor tag to the github, resume and linkedin
+// closing tags for these
+
+// export default Footer
