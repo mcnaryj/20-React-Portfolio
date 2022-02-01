3e6cf2f1955e714142243f5a2d7281fddd71d1f4
Jack McNary <jack.mcnary@gmail.com>
2022-02-01 15:07:29 -0700

    adding index.html to public folder

--- src/index.js
+++ src/index.js
@@ -3,11 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
-// react DOM render (
-// <Reactstrict.mode <app (self-closing) </strict
-// doc get el by id root
-// );
 ReactDOM.render(
     <React.StrictMode>
         <App />
