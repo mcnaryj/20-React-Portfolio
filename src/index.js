44b5dfb465a0f38d11ead73dd5a4dc8c485bf2bf
Jack McNary <jack.mcnary@gmail.com>
2022-02-01 14:22:50 -0700

    Building out app.jsx

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+
+// react DOM render (
+// <Reactstrict.mode <app (self-closing) </strict
+// doc get el by id root
+// );
