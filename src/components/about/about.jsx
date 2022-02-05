dd7060fa5f40b89e7d26c06e306ad2cedd3d8ab2
Jack McNary <jack.mcnary@gmail.com>
2022-02-05 16:09:34 -0700

    building out about-me

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
 
-export default function About() {
+const About = () => {
     return (
         <div>
-            <h1>About Page</h1>
-            <p>
-                Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-                Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-                mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-                lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-                imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-                in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-            </p>
-        </div>
+            <div className="leftSide">
+                <div className="myName">Jack McNary</div>
+                <div className="jobTitle">Technical Account Manager</div>
+                <div className="aboutMe">
+                    I am a Technical Account Manager at Straker Translations, a leading language-service provider (LSP). I'm currently taking a coding bootcamp through the University of Denver, with the aim of using these skills to further my professional career, though that's not necessarily through coding, per se!
+                    I thoroughly enjoy my current position at Straker, where I feel I am highly valued for my ability to solve problems, think critically and act decisively, especially in situations where not all the key pieces of information are available. I act as a liaison between our production/development teams and the client, and my main goal in learning coding is to be able to speak "developer" in addition to the other languages, Spanish and Portuguese, that I have learned through previous studies.
+                </div>
+            </div>
+            <div className='rightSide'>
+                <img>Image of me</img>
+            </div>
+        </div >
+
     );
 }
+
+
+export default About
