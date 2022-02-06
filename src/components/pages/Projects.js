62a787ef7614cff4f8fb2cc28a9d7401681ca7ee
Jack McNary <jack.mcnary@gmail.com>
2022-02-05 22:26:52 -0700

    that is enough for today

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -4,50 +4,73 @@ import gentleTilly from '../../images/gentle-tilly.png'
 import noteTaker from '../../images/Note-Taker.png'
 import employeeTracker from '../../images/Employee-tracker.png'
 import eCommerce from '../../images/E-Commerce.png'
+import socialNetwork from '../../images/Social_network_api.png'
+
 import './projects.css';
+
 
 // import css
 function Project() {
     return (
-        <section className='projects'>
-            <div className="card">Marvellisms</div>
-            <img src={Marvellisms} className="first-image" alt="Marvellisms, the Marvel Database" />
-            <a href="https://carlincb.github.io/Marvelisms/" target="_blank" rel="noreferrer">
-                <section className="marvellisms">
-                    <header>Marvellisms - The
-                        Marvel Database
-                    </header>
-                </section>
-            </a>
-            <section className="other-projects">
-                <a href="https://www.youtube.com/watch?v=Xc_GST9Am94&feature=youtu.be." target="_blank" rel="noreferrer">
-                    <h4 className="title">E-Commerce Back End</h4>
-                    <img src={eCommerce} className="other-images" alt="e-Commerce database" />
-                    <section className="e-commerce">
 
-                    </section>
-                </a>
-                <a href="https://peaceful-reef-89877.herokuapp.com/" target="_blank" rel="noreferrer">
-                    <img src={noteTaker} className="other-images" alt="Note Taker" />
-                    <h4 className="title">Note Taker App</h4>
-                    <section className="note-taker">
-                    </section>
-                </a>
-                <a href="https://github.com/Valllerian/Gentle-Tilly/" target="_blank" rel="noreferrer">
-                    <img src={gentleTilly} className="other-images" alt="gentle tilly" />
-                    <h4 className="title">Gentle Tilly!</h4>
-                    <section className="gentle-tilly">
-                    </section>
-                </a>
-                <a href="https://www.youtube.com/watch?v=66R85GNfabY" target="_blank" rel="noreferrer">
-                    <img src={employeeTracker} className="other-images" alt="employee tracker" />
-                    <h4 className="title">Employee Database Tracker</h4>
-                    <section className="employee-tracker">
-                        <header>Employee Database Tracker</header>
-                    </section>
-                </a>
-            </section>
-        </section >
+        <div className='projects'>
+            <div className="card" style={{ width: "10%" }}>
+                <img src={socialNetwork} className="first-image" alt="Facebook sucks" />
+                <div className="body">
+                    <h6 className="title">Social Network API</h6>
+                    <div className='card-description'>The newest and coolest social network - Mark Zuckerberg, eat your robot-lizard heart out!</div>
+                    <a href="https://watch.screencastify.com/v/xWw3HyvOnYrEolDr9ar9" target="_blank" rel="noreferrer">Check it out!
+                    </a>
+                </div>
+            </div>
+            <div className="card" style={{ width: "10%" }}>
+                <img src={Marvellisms} className="first-image" alt="Marvellisms, the Marvel Database" />
+                <div className="body">
+                    <h6 className="title">Marvellisms</h6>
+                    <div className='card-description'>Marvellisms - The
+                        Marvel Database</div>
+                    <a href="https://carlincb.github.io/Marvelisms/" target="_blank" rel="noreferrer">Check it out!
+                    </a>
+                </div>
+            </div>
+            <div className="card" style={{ width: "10%" }}>
+                <img src={employeeTracker} className="first-image" alt="Employee Database Tracker" />
+                <div className="body">
+                    <h6 className="title">Employee Tracker Database</h6>
+                    <div className='card-description'>Keep track of your employees with this marvelous tracker.</div>
+                    <a href="https://www.youtube.com/watch?v=66R85GNfabY" target="_blank" rel="noreferrer">Check it out!
+                    </a>
+                </div>
+            </div>
+            <div className="card" style={{ width: "10%" }}>
+                <img src={gentleTilly} className="first-image" alt="Gentle Tilly!" />
+                <div className="body">
+                    <h6 className="title">Gentle Tilly</h6>
+                    <div className='card-description'>Check out any upcoming hockey game and also talk trash with your friends.</div>
+                    <a href="https://github.com/Valllerian/Gentle-Tilly/" target="_blank" rel="noreferrer">Check it out!
+                    </a>
+                </div>
+            </div>
+            <div className="card" style={{ width: "10%" }}>
+                <img src={noteTaker} className="first-image" alt="Note-Taker App" />
+                <div className="body">
+                    <h6 className="title">Note Taker</h6>
+                    <div className='card-description'>Take hella notes with this note taker app!</div>
+                    <a href="https://peaceful-reef-89877.herokuapp.com/" target="_blank" rel="noreferrer">Check it out!
+                    </a>
+                </div>
+            </div>
+            <div className="card" style={{ width: "10%" }}>
+                <img src={eCommerce} className="first-image" alt="E Commerce Magick" />
+                <div className="body">
+                    <h6 className="title">E-Commerce Back End</h6>
+                    <div className='card-description'>The magick of eCommerce!</div>
+                    <a href="https://www.youtube.com/watch?v=Xc_GST9Am94&feature=youtu.be." target="_blank" rel="noreferrer">Check it out!
+                    </a>
+                </div>
+            </div>
+
+        </div >
     );
 };
 
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,55 +1,23 @@
 import React from 'react';
+import myResume from '../../images/JM_Resume.pdf';
+import straker from '../../images/straker.png'
+import resumeThumbnail from '../../images/resume_thumbnail.png'
 import './resume.css';
 
 const Resume = () => {
     return (
-        <div className="resume-container">
-            <div className="resume-child">
-                <h1>Career History and Accomplishments</h1>
-                <h2>Straker Translations, Ltd., Denver, CO, September 2017-Present</h2>
-                <h3>Technical Account Manager/Inside Sales Representative</h3>
-                <ul>• Attained over 100% of yearly target revenue six months into the 2021 fiscal year</ul>
-                <ul> • Works closely with the New Business and Client Success teams, providing technical support where needed to ensure smooth functioning of the team as a whole</ul>
-                <ul>• Provides technical support to the Client Success team, alongside ongoing account management duties</ul>
-                <h3>Account Supervisor/Project Manager</h3>
-                <ul>• Responsible for managing ongoing translations for an ITAR Compliant US Defense Contractor, representing revenue of over 400,000 USD and over 2 million words translated over one year</ul>
-                <ul> • Lead contact for the translation of materials for an intensive IT Certificate course, eventually realizing a profit margin of 70%, representing a profit of 70,000 USD</ul>
-                <ul> • Provided client service including quote processing, document verification, project updates</ul>
-                <ul>   • Managed a high volume of projects increasing in complexity with time on the job. Successive internal promotions based upon performance in these roles</ul>
-                <h2>Alfalfa’s Market</h2>
-                <h3>Supervisor, Cheese and Specialty Department, Boulder, CO, November 2015-August 2017</h3>
-                <ul>• Provided a complete customer experience utilizing extensive product knowledge and accompanying techniques, emphasizing communication with the customer to best identify and serve their needs</ul>
-                <h2>CHaRM (Center for Hard-to-Recycle Materials), Eco-Cycle</h2>
-                <h3>Cashier/Yard Attendant, Boulder, CO, November 2015-October 2016</h3>
-                <ul>• Responsible for educating the body public on good recycling practices, giving tours of the facilities, and promoting related programs, in addition to cashier and intake roles</ul>
+        <div className="resume-child">
+            <div className='resume-child'>Click on the icon to download my resume:<a href={myResume} download rel="noreferrer"><img src={resumeThumbnail} alt="it's a thumbnail" className='thumbnail'></img> </a></div>
+            <div className='resume-child'><img src={straker} alt='my master' className='straker'></img></div>
+
+            <h2>Proficiencies</h2>
+            <div>I'm not going to make some bold claim and say I am really proficient in any of the topics that we've discussed in this class, however I do feel comfortable operating within the MERN stack, specifically with Node.js, Express and somewhat with React.
             </div>
-            <div className='resume-child'>
-                <h1>Education</h1>
-                <p>University of Denver Coding Bootcamp, Certificate, completion date February 2022</p>
-                <p> University of Colorado, Boulder, CO, B.A., International Affairs, Minor in Portuguese, December 2014</p>
-                <h1>Volunteerism</h1>
-                <p>Creek-path clean-up campaign (self-directed), Boulder, CO, December 2021-Ongoing</p>
-                <p>Wilderness Restoration Fund, Boulder County, CO, Ongoing</p>
-                <p>University of Colorado Recycling Center, Boulder, CO, May-July 2012</p>
-                <p>Volunteer Ski Patrol, Winter Park Ski Resort, Winter Park, CO, August 2010-December 2012</p>
-                <h2>What I Offer</h2>
-                <ul>• Excellent communication skills, both verbal and written</ul>
-                <ul>• Team player, who enjoys building trust with my colleagues and developing robust client relationships</ul>
-                <ul>• Not afraid to get my hands dirty to resolve issues before they become problematic</ul>
-                <ul>• Technical prowess with multiple software platforms; eagerness to learn and familiarize myself with new systems and processes</ul>
-                <ul>• Proficient in reading and comprehension of Portuguese, Spanish, and French</ul>
-                <ul>• Comfortable in high-pressure situations in and outside of work</ul>
-                <h2>Interests</h2>
-                <ul>• Well-traveled, with a keen interest in learning about and understanding different cultures</ul>
-                <ul> • Passionate about proper recycling practices and being an eco-conscious citizen</ul>
-                <ul>• Frequent collaborator with the Portuguese and Spanish Department at CU Boulder</ul>
-                <ul>• Freelance writer: authored articles for GetSkiTickets.com received the highest traffic of any on the site</ul>
-                <ul>• Avid skier, sports enthusiast, musician, and all-around good guy</ul>
+            <div>Aside from that, I feel comfortable using HTML, CSS (if push came to shove, I would make this website look real nice, but it is simply not within my bandwidth right now given the other things I am currently pursuing in life), JavaScript, and I think I could learn most languages without too much difficulty.
             </div>
 
 
-        </div>
-
+        </div >
     )
 }
 
