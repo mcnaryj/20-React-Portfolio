2eadde6c6078e4e87ecd9f3fc048ae9fa2570c80
Jack McNary <jack.mcnary@gmail.com>
2022-02-08 21:12:24 -0700

    adding css to contact form

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,6 +1,7 @@
 import React, { useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
 import { validateEmail } from '../../utils/helpers';
+import '../pages/contact.css'
 
 function Contact() {
 
